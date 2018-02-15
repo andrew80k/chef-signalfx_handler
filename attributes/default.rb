44911802ba03cff2e1f020af034ee344a8c1d355
andrew80k <andrew80k@gmail.com>
2018-02-15 11:58:22 -0500

    Update default.rb

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,4 @@
-default["chef_signalfx"]["token"] = 'NdRd2daQungsXmALu3-uGA'
+default["chef_signalfx"]["token"] = 'YOUR_TOKEN_NUMBER_HERE'
 default["chef_signalfx"]["dimensions"] = [
   {:key => 'environment', :value => node.chef_environment}
 ]
